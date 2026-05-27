005fa7ef5521c7007779b57facbf953de5a95992
BALAJI V <100275639+Balaji-2001@users.noreply.github.com>
2026-05-27 12:51:05 +0530

    Delete task2_pipeline_building/Summary_query.sql

--- task2_pipeline_building/Summary_query.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  city,
-  temp_band,
-  COUNT(*) AS hour_count,
-  AVG(temperature_2m) AS avg_temperature,
-  MAX(wind_speed_10m) AS max_wind_speed
-FROM `your-project.your_dataset.weather_forecast`
-GROUP BY city, temp_band
-ORDER BY city, temp_band;
