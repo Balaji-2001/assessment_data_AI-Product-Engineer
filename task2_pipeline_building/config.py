04e7fa48af563398ca4355acf7d9334940c30a76
BALAJI V <100275639+Balaji-2001@users.noreply.github.com>
2026-05-27 12:48:59 +0530

    Create config.py

---
+++ task2_pipeline_building/config.py
@@ -0,0 +1,7 @@
+CITIES = [
+    {"city": "Chennai", "latitude": 13.0827, "longitude": 80.2707},
+    {"city": "Bengaluru", "latitude": 12.9716, "longitude": 77.5946},
+]
+
+HOURLY_FIELDS = "temperature_2m,relative_humidity_2m,wind_speed_10m"
+API_URL = "https://api.open-meteo.com/v1/forecast"
